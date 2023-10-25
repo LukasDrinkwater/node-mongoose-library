b00845901ef055c5ab758456da21e7066a542559
Lukas <lukasdrinkwater97@gmail.com>
2023-10-25 20:28:41 +0100

    setting up base nodejs project, started connecting to mongoDB atlas

--- expres-Project-Mini-Message-Board/routes/users.js
+++ expres-Project-Mini-Message-Board/routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+router.get("/profile", function (req, res, next) {
+  res.send("profile page");
+});
+
+router.get("/cool", function (req, res, next) {
+  res.render("cool", { title: "You're so cool" });
 });
 
 module.exports = router;
