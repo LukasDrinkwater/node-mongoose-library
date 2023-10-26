fa9e073d1342837f856e2306bc14720f15de05dd
Lukas <lukasdrinkwater97@gmail.com>
2023-10-26 18:14:14 +0100

    set up models

--- expres-Project-Mini-Message-Board/app.js
+++ expres-Project-Mini-Message-Board/app.js
@@ -1,13 +1,27 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const { mongo } = require("mongoose");
 
-var app = express();
+// mongodb+srv://Lukas:Dolomite17@cluster0.5xartib.mongodb.net/local_library?retryWrites=true&w=majority
+
+const app = express();
+
+// setup the mongoose mongoDB connection
+const mongoose = requrie("mongoose");
+mongoose.set("strictQuery, false");
+const mongoDB =
+  "mongodb+srv://Lukas:Dolomite17@cluster0.5xartib.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
